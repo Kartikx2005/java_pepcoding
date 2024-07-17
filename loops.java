698d0215e1f82fb027ec67ad5c06ecf8300672cc
Kartikx2005 <kartikxdev2005@gmail.com>
2024-07-17 19:08:38 +0530

    completed loops

---
+++ loops.java
@@ -0,0 +1,18 @@
+class loops{
+    public static void main(String[] args){
+        // while loop 
+
+        // int i = 1;
+        // while(i<=9){
+        //     System.out.println(i);
+        //     i++;
+        // }
+        // System.out.println("Done !");
+
+        // for loop 
+        
+        for(int i = 0; i<=9; i++){
+            System.out.println(i);
+        }
+    }
+}
