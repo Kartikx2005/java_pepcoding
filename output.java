b431cfe77f0d06f602347a0b277ea10aab5debd2
Kartikx2005 <kartikxdev2005@gmail.com>
2024-07-17 18:34:19 +0530

    completed output

---
+++ output.java
@@ -0,0 +1,24 @@
+class output{
+    public static void main(String[] args){
+        System.out.print("This is first Line");
+        System.out.println("Hello World"); // move to new line after printing
+
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+
+        // System.out.println("*\n**\n***\n****\n***** ");
+
+        // print z 
+        System.out.println("*****");
+        System.out.println("   * ");
+        System.out.println("  *  ");
+        System.out.println(" *   ");
+        System.out.println("*****");
+
+        // System.out.print("*****\n   * \n  *  \n *   \n*****");
+
+    }
+}
