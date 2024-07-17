cc34b1cea5440a852ae5e03144838cbc41ddc05a
Kartikx2005 <kartikxdev2005@gmail.com>
2024-07-17 18:34:31 +0530

    completed variables

---
+++ variables.java
@@ -0,0 +1,22 @@
+class variables{
+    public static void main(String[] args){
+        int x = 10;
+        int y = 15;
+        int sum = x + y;
+        System.out.println("Sum of " + x + " and " + y + " is " + sum);
+
+        int prd = x * y;
+        System.out.println("Product of " + x + " and " + y + " is " + prd);
+
+        int v1 = x/y;
+        int v2 = y/x;
+        int v3 = x%y;
+        System.out.println(v1);
+        System.out.println(v2);
+        System.out.println(v3);
+
+
+        // (* % /) (Same Priority) > (+ -) (Same Priority)
+        
+    }
+}
